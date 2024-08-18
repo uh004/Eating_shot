515ca603d77c266a79a4dcbed2619f8f081d17f5
Jonggwan Park <craftguy55@gmail.com>
2024-08-18 13:52:50 +0900

    creating another form

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.forms import Form
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ["username", "email", "password1"]
+        fields = ["username", "email", "password1", "password2"]
 
 
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
         model = CustomUser
+
+
+class SomethingFrom(Form):
+    pass
