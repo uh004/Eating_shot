8533adc094875415ca43357f20170bac172b0afa
Jonggwan Park <craftguy55@gmail.com>
2024-09-08 21:49:42 +0900

    juggling ideas for testing django application

---
+++ djangoapp/tests/test_transactions.py
@@ -0,0 +1,17 @@
+# tests/test_transactions.py
+from django.db import transaction
+from django.test import TestCase
+from users.models import BloodSugar
+
+
+class TransactionTest(TestCase):
+    def test_atomic_transaction(self):
+        try:
+            with transaction.atomic():
+                BloodSugar.objects.create(
+                    user_id=1, time="morning", date="2023-01-01", blood_sugar=120
+                )
+                raise Exception("Forced exception")
+        except Exception:
+            pass
+        self.assertEqual(BloodSugar.objects.count(), 0)
