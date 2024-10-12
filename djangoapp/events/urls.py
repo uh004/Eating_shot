ed7bf60438e9610f8afbc4c31b2033a622199f3e
Jonggwan Park <craftguy55@gmail.com>
2024-10-12 18:46:13 +0900

    disable predefined set of channels

--- djangoapp/events/urls.py
+++ djangoapp/events/urls.py
@@ -3,6 +3,6 @@ import django_eventstream
 from . import views
 
 urlpatterns = [
-    path("", include(django_eventstream.urls), {"channels": ["alarms"]}),
+    path("", include(django_eventstream.urls)),
     path("trigger_test_alarm/", views.trigger_test_alarm, name="trigger_test_alarm"),
 ]
